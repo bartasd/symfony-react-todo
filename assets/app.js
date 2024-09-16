7e64d28eb3d957c67554958407c2edb05c422dae
Bartas Dausynas <bartas3@gmail.com>
2024-09-16 18:27:07 +0300

    added some basic mui table

--- assets/app.js
+++ assets/app.js
@@ -1,32 +1,63 @@
 import React, { useContext } from "react";
 import ReactDOM from "react-dom/client";
 import { TodoContext, TodoContextWrapper } from "./contexts/TodoContext";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
 
 function App() {
   const { someNumber, setSomeNumber } = useContext(TodoContext);
 
   return (
-    <div>
-      <p>SOME VALUE: {someNumber}</p>
-      <div
-        onClick={() => setSomeNumber(someNumber + 5)}
-        style={{ cursor: "pointer", color: "blue" }}
-      >
-        INCREASE BY 5
-      </div>
-      <div
-        onClick={() => setSomeNumber(someNumber - 5)}
-        style={{ cursor: "pointer", color: "blue" }}
-      >
-        DECREASE BY 5
-      </div>
-      <div
-        onClick={() => setSomeNumber(0)}
-        style={{ cursor: "pointer", color: "red" }}
-      >
-        RESET
-      </div>
-    </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
