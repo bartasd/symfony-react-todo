fd44cb1658ddf0963b137c7a9e9a8a389b61f6c9
Bartas Dausynas <bartas3@gmail.com>
2024-09-25 12:26:04 +0300

    introduced editing/delete

--- assets/app.js
+++ assets/app.js
@@ -14,15 +14,32 @@ import { IconButton, TextField } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import CheckIcon from "@mui/icons-material/Check";
 
 function App() {
+  const [editing, setEditing] = useState(-1);
   const { todos, setTodos } = useContext(TodoContext);
   const [addTodo, setAddTodo] = useState("");
+  const [editTodo, setEditTodo] = useState("");
+
   function putTodo(event) {
     event.preventDefault();
     setTodos((old) => [...old, { name: addTodo }]);
     setAddTodo("");
   }
+  function handleEditTodo(event) {
+    event.preventDefault();
+    setTodos((old) =>
+      old.map((e, i) => (i === editing ? { name: editTodo } : e))
+    );
+    setEditTodo("");
+    setEditing(-1);
+  }
+
+  function deleteTodo(idx) {
+    setTodos((old) => old.filter((_, i) => i !== idx));
+  }
+
   return (
     <form onSubmit={putTodo}>
       <Table>
@@ -39,8 +56,8 @@ function App() {
                 value={addTodo}
                 onChange={(e) => setAddTodo(e.target.value)}
                 label="New Task"
-                fullWidth={true}
-              ></TextField>
+                fullWidth
+              />
             </TableCell>
             <TableCell align="right">
               <IconButton type="submit">
@@ -49,15 +66,40 @@ function App() {
             </TableCell>
           </TableRow>
           {todos.map((todo, idx) => (
-            <TableRow key={`todo: ${idx}`}>
-              <TableCell>{todo.name}</TableCell>
+            <TableRow key={`todo-${idx}`}>
+              <TableCell>
+                {editing === idx ? (
+                  <form onSubmit={handleEditTodo}>
+                    <TextField
+                      value={editTodo}
+                      onChange={(e) => setEditTodo(e.target.value)}
+                      fullWidth
+                    />
+                  </form>
+                ) : (
+                  todo.name
+                )}
+              </TableCell>
               <TableCell align="right">
-                <IconButton>
-                  <EditIcon />
-                </IconButton>
-                <IconButton>
-                  <DeleteIcon />
-                </IconButton>
+                {editing === idx ? (
+                  <IconButton onClick={handleEditTodo}>
+                    <CheckIcon />
+                  </IconButton>
+                ) : (
+                  <>
+                    <IconButton
+                      onClick={() => {
+                        setEditTodo(todo.name);
+                        setEditing(idx);
+                      }}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton onClick={() => deleteTodo(idx)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </>
+                )}
               </TableCell>
             </TableRow>
           ))}
